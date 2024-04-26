4e6bdf29b5a991ec5865fe618198c4b9ca508f52
Anderson Alves <154446733+andersonkks@users.noreply.github.com>
2024-04-25 21:34:07 -0300

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ function login(event) {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    if (email === 'andersonavcontato@gmail.com' && senha === 'dash135') {
+    if (email === 'vivianeoliver805@gmail.com' && senha === 'dash135') {
         localStorage.setItem('loggedIn', true);
         window.location.href = 'agenda.html';
     } else {
