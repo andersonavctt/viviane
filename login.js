4cf3f7e37c94acf3f4dbbd91eafed1eb20bfc2e7
Anderson Alves <154446733+andersonkks@users.noreply.github.com>
2024-04-25 21:58:36 -0300

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ function login(event) {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    if (email === 'vivianeoliver805@gmail.com' && senha === 'dash135') {
+    if (email === 'vivianeoliver805@gmail.com') {
         localStorage.setItem('loggedIn', true);
         window.location.href = 'agenda.html';
     } else {
@@ -18,5 +18,41 @@ function login(event) {
     }
 }
 
+function sendResetEmail(email) {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Port: 2525,
+        Username: "andersonavcontato@gmail.com",
+        Password: "1AD9353E82F0E290C67E86EC2A7E42806624",
+        To: email,
+        From: "andersonavcontato@gmail.com",
+        Subject: "Redefinição de Senha",
+        Body: "Clique no link abaixo para redefinir sua senha: <a href='https://seusite.com/reset_password'>Redefinir Senha</a>",
+    }).then(
+        message => alert("Email de redefinição de senha enviado com sucesso.")
+    );
+}
+
+function forgotPassword(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    
+    sendResetEmail(email);
+
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('reset-password-form').style.display = 'block';
+}
+
+function resetPassword(event) {
+    event.preventDefault();
+    const newPassword = document.getElementById('new-password').value;
+    
+    alert('Sua senha foi redefinida com sucesso.');
+
+    window.location.href = 'index.html';
+}
+
 checkLogin();
 document.getElementById('login-form').addEventListener('submit', login);
+document.getElementById('forgot-password').addEventListener('click', forgotPassword);
+document.getElementById('reset-password-form').addEventListener('submit', resetPassword);
