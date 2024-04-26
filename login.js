6f735f563299265d98f73a79ab65e5ea03b6addb
Anderson Alves <154446733+andersonkks@users.noreply.github.com>
2024-04-25 22:17:34 -0300

    Update login.js

--- login.js
+++ login.js
@@ -10,49 +10,45 @@ function login(event) {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    if (email === 'vivianeoliver805@gmail.com') {
-        localStorage.setItem('loggedIn', true);
-        window.location.href = 'agenda.html';
+    const storedHash = localStorage.getItem(email);
+    if (storedHash) {
+        bcrypt.compare(senha, storedHash, function(err, result) {
+            if (result) {
+                localStorage.setItem('loggedIn', true);
+                window.location.href = 'agenda.html';
+            } else {
+                alert('Email ou senha incorretos. Tente novamente.');
+            }
+        });
     } else {
-        alert('Email ou senha incorretos. Tente novamente.');
+        alert('Usuário não encontrado.');
     }
-}
-
-function sendResetEmail(userEmail) {
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Port: 2525,
-        Username: "andersonavcontato@gmail.com",
-        Password: "1AD9353E82F0E290C67E86EC2A7E42806624",
-        To: userEmail,
-        From: "andersonavcontato@gmail.com",
-        Subject: "Redefinição de Senha",
-        Body: "Clique no link abaixo para redefinir sua senha: <a href='https://seusite.com/reset_password'>Redefinir Senha</a>",
-    }).then(
-        message => alert("Email de redefinição de senha enviado com sucesso.")
-    );
 }
 
 function forgotPassword(event) {
     event.preventDefault();
-    const userEmail = document.getElementById('email').value;
-    
-    sendResetEmail(userEmail);
-
     document.getElementById('login-form').style.display = 'none';
     document.getElementById('reset-password-form').style.display = 'block';
 }
 
 function resetPassword(event) {
     event.preventDefault();
+    const email = document.getElementById('email').value;
     const newPassword = document.getElementById('new-password').value;
-    
-    alert('Sua senha foi redefinida com sucesso.');
 
-    window.location.href = 'index.html';
+    bcrypt.hash(newPassword, 10, function(err, hash) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        localStorage.setItem(email, hash);
+        alert('Sua senha foi redefinida com sucesso.');
+        window.location.href = 'index.html';
+    });
 }
 
-checkLogin();
-document.getElementById('login-form').addEventListener('submit', login);
+document.getElementById('loginForm').addEventListener('submit', login);
 document.getElementById('forgot-password').addEventListener('click', forgotPassword);
-document.getElementById('reset-password-form').addEventListener('submit', resetPassword);
+document.getElementById('resetPasswordForm').addEventListener('submit', resetPassword);
+
+checkLogin();
